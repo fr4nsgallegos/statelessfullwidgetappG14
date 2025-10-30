910312930d7ef07b71511ffd8117523f59f03db5
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-10-29 21:41:27 -0500

    stateles widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:statelessfullwidgetapp/pages/home_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage()));
+  runApp(MaterialApp(home: MiWidgetEstatico()));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class MiWidgetEstatico extends StatelessWidget {
+  String textoMostrar = "Hola soy un StatelesWidget";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Aplicativo de estados")));
+    return Scaffold(
+      appBar: AppBar(title: Text("Aplicativo de estados")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(textoMostrar, style: TextStyle(fontSize: 30)),
+            ElevatedButton(
+              onPressed: () {
+                textoMostrar = "hola como estas";
+                print(textoMostrar);
+              },
+              child: Text("Cambiar el valor de la variable a hola como estas"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
