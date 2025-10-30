2535cd0bbba7a38d62bc0334a15a63b18d2aaa61
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-10-29 22:02:03 -0500

    statefull

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,41 @@
 import 'package:flutter/material.dart';
 
+class MiWidgetStateFull extends StatefulWidget {
+  const MiWidgetStateFull({super.key});
+
+  @override
+  State<MiWidgetStateFull> createState() => _MiWidgetStateFullState();
+}
+
+class _MiWidgetStateFullState extends State<MiWidgetStateFull> {
+  String texto = "Hola soy un statefullwidget";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              texto,
+              style: TextStyle(fontSize: 35),
+              textAlign: TextAlign.center,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                texto = "Hola este es el nuevo valor";
+                print(texto);
+                setState(() {});
+              },
+              child: Text("Cambiar texto"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class MiWidgetEstatico extends StatelessWidget {
   String textoMostrar = "Hola soy un StatelesWidget";
 
